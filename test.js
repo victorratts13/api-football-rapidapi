3a44f3bfbda7dee7048ba282fd7f1958f4b3c429
Victor Ratts <victor.ratts13@gmail.com>
2020-03-10 16:34:14 -0300

    Update test.js

--- test.js
+++ test.js
@@ -5,7 +5,7 @@
 //console.log(get)
 
 const test = require('./app');
-const Test = new test('fec9660e1fmsha52ec2f330972a6p1b3664jsnbdbf86af7e58');
+const Test = new test('API_KEY');
 
 Test.get('v2/fixtures/live', '?').then((res) => {
     console.log(JSON.stringify(res, true, 1))
