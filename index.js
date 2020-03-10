acdd69efc8ea502b3255e05d897d7200ce779ebc
Victor Ratts <victor.ratts13@gmail.com>
2020-03-10 16:38:02 -0300

    add v 1.0.2

--- index.js
+++ index.js
@@ -12,7 +12,14 @@ class ApiFootball {
     get(endPoint, Params){
         return new Promise((resolve, reject) => {
         if (!endPoint) return reject(new Error('Endpoint cannot be empty'))
-
+   /**
+   * Para obter a lista de todos os pontos de extremidade disponíveis, visite a documentação da API https://rapidapi.com/api-sports/api/api-football
+   *
+   * @param {string} endpoint - The endpoint name, i.e.: 'v2/fixtures/live', 'v2/fixtures/date/2020-02-06'
+   * @param {string} params - Parâmetros de consulta a serem anexados à solicitação (se houver). Exemplos: {fuso horário: 'Europa / Londres'}
+   * @return {Promise} Resposta API
+   *
+   */
             //console.log(baseURI);
             //console.log(baseURL);
             //console.log(endPoint);
